69a900382c2e84918866e5c8411718ece664c245
Christian Jackson-Gruber <christian@geekinasuit.com>
2025-05-09 10:17:51 -0700

    Finish it up.

--- kotlin/simple_ocr/src/main/kotlin/me/cgruber/dl/ocr/ocr.kt
+++ kotlin/simple_ocr/src/main/kotlin/me/cgruber/dl/ocr/ocr.kt
@@ -1,13 +1,15 @@
 package me.cgruber.dl.ocr
 
 import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.core.ProgramResult
 import com.github.ajalt.clikt.core.main
 import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.parameters.options.convert
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
+import java.io.ByteArrayOutputStream
 import java.io.File
+import java.io.PrintStream
+import java.lang.Math.round
 import org.jetbrains.kotlinx.dl.api.core.Sequential
 import org.jetbrains.kotlinx.dl.api.core.WritingMode
 import org.jetbrains.kotlinx.dl.api.core.layer.core.Dense
@@ -20,8 +22,6 @@ import org.jetbrains.kotlinx.dl.api.inference.TensorFlowInferenceModel
 import org.jetbrains.kotlinx.dl.api.summary.printSummary
 import org.jetbrains.kotlinx.dl.dataset.OnHeapDataset
 import org.jetbrains.kotlinx.dl.dataset.embedded.fashionMnist
-import java.io.ByteArrayOutputStream
-import java.io.PrintStream
 
 class Ocr : CliktCommand(name = "ocr") {
   override fun run() {}
@@ -42,15 +42,15 @@ class Train : CliktCommand() {
         // comment to keep vertical style
         Input(28, 28, 1),
         Flatten(),
-        Dense(100),
-        Dense(50),
+        Dense(500),
+        Dense(200),
         Dense(10),
       )
     model.use {
       it.compile(
         optimizer = Adam(),
         loss = Losses.SOFT_MAX_CROSS_ENTROPY_WITH_LOGITS,
-        metric = Metrics.ACCURACY
+        metric = Metrics.ACCURACY,
       )
       val buf = ByteArrayOutputStream()
       it.printSummary(PrintStream(buf))
@@ -58,35 +58,43 @@ class Train : CliktCommand() {
       echo(buf.toString())
 
       // You can think of the training process as "fitting" the model to describe the given data :)
-      it.fit(
-        dataset = training,
-        epochs = 10,
-        batchSize = 100
-      )
+      it.fit(dataset = training, epochs = 10, batchSize = 100)
 
       val accuracy = it.evaluate(dataset = test, batchSize = 100).metrics[Metrics.ACCURACY]
 
       echo("Accuracy: $accuracy")
       it.save(modelFile, writingMode = WritingMode.OVERRIDE)
+      echo("Writing model file to ${modelFile.absolutePath}")
     }
   }
 }
 
 class Run : CliktCommand() {
+  val dir by option("--dir", "-d", help = "Cache directory").convert { File(it) }.default(File("."))
+  val modelFile by
+    option("--model", "-m", help = "The model persistence file")
+      .convert { File(it) }
+      .default(File("models/my.model"))
+
   override fun run() {
     echo("Loading testing data")
     val (_, test: OnHeapDataset) = fashionMnist()
 
-    TensorFlowInferenceModel.load(File("model/my_model")).use {
+    TensorFlowInferenceModel.load(modelFile).use {
       it.reshape(28, 28, 1)
-
-      val prediction = it.predict(test.getX(0))
-      val actualLabel = test.getY(0)
-
-      println(
-        "Predicted label is: $prediction. This corresponds to class ${stringLabels[prediction]}."
-      )
-      println("Actual label is: $actualLabel.")
+      var correct = 0
+      val attempts = test.xSize()
+      for (i in 0..attempts - 1) {
+        val prediction = it.predict(test.getX(i))
+        val actualLabel = test.getY(i)
+        if (round(actualLabel) == prediction) correct += 1
+        //        echo(
+        //          "Predicted label is: $prediction. " +
+        //            "This corresponds to class ${stringLabels[prediction]}."
+        //        )
+        //        echo("Actual label is: $actualLabel.")
+      }
+      echo("Predictions correctly guessed $correct/$attempts items.")
     }
   }
 }
